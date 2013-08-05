6b1d56ce498524b3b770e6eb33a71de2611f7b0e
Mike Koepke <mike.koepke@gmail.com>
2013-08-05 13:12:01 -0500

    PHP 5.4 compat.  Fix Strict errors

--- widget-utils.php
+++ widget-utils.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * Widget Utils
- * Author: Denis de Bernardy <http://www.mesoconcepts.com>
- * Version: 2.0.1
+ * Author: Denis de Bernardy & Mike Koepke <http://www.semiologic.com>
+ * Version: 2.1
  */
 
 /**
@@ -17,87 +17,87 @@ class widget_utils {
 	 *
 	 * @return void
 	 **/
-	
-	function post_meta_boxes() {
+
+	static function post_meta_boxes() {
 		static $done = false;
-		
+
 		if ( $done )
 			return;
-		
+
 		add_meta_box('post_widget_config', __('This Post In Widgets', widget_utils_textdomain), array('widget_utils', 'post_widget_config'), 'post');
 		add_action('save_post', array('widget_utils', 'post_save_widget_config'), 0);
 
 		$done = true;
 	} # post_meta_boxes()
-	
-	
+
+
 	/**
 	 * page_meta_boxes()
 	 *
 	 * @return void
 	 **/
-	
-	function page_meta_boxes() {
+
+	static function page_meta_boxes() {
 		static $done = false;
-		
+
 		if ( $done )
 			return;
-		
+
 		add_meta_box('page_widget_config', __('This Page In Widgets', widget_utils_textdomain), array('widget_utils', 'page_widget_config'), 'page');
 		add_action('save_post', array('widget_utils', 'page_save_widget_config'));
-		
+
 		$done = true;
 	} # page_meta_boxes()
-	
-	
+
+
 	/**
 	 * post_widget_config()
 	 *
 	 * @param object $post
 	 * @return void
 	 **/
-	
+
 	function post_widget_config($post) {
 		widget_utils::widget_config('post', $post);
 	} # post_widget_config()
-	
-	
+
+
 	/**
 	 * page_widget_config()
 	 *
 	 * @param object $post
 	 * @return void
 	 **/
-	
-	function page_widget_config($post) {
+
+    static function page_widget_config($post) {
 		widget_utils::widget_config('page', $post);
 	} # page_widget_config()
-	
-	
+
+
 	/**
 	 * post_save_widget_config()
 	 *
 	 * @param int $post_ID
 	 * @return void
 	 **/
-	
-	function post_save_widget_config($post_ID) {
+
+	static function post_save_widget_config($post_ID) {
 		widget_utils::save_widget_config($post_ID, 'post');
 	} # post_save_widget_config()
-	
-	
+
+
 	/**
 	 * page_save_widget_config()
 	 *
 	 * @param int $post_ID
 	 * @return void
 	 **/
-	
-	function page_save_widget_config($post_ID) {
+
+    static function page_save_widget_config($post_ID) {
 		widget_utils::save_widget_config($post_ID, 'page');
 	} # page_save_widget_config()
-	
-	
+
+
 	/**
 	 * widget_config()
 	 *
@@ -105,7 +105,7 @@ class widget_utils {
 	 * @param object $post
 	 * @return void
 	 **/
-	
+
 	function widget_config($type, $post) {
 		$post_ID = $post->ID;
 
@@ -116,13 +116,13 @@ class widget_utils {
 		echo '<ul class="ul-disc">';
 		do_action($type . '_widget_config_affected');
 		echo '</ul>' . "\n";
-		
+
 		echo '<p>'
 			. __('It will <b>NOT</b> affect anything else. In particular WordPress\'s built-in Pages widget.', widget_utils_textdomain)
 			. '</p>' . "\n";
-		
+
 		echo '<table style="width: 100%;">';
-		
+
 		echo '<tr valign="top">' . "\n"
 			. '<th scope="row" width="120px;">'
 			. __('Title', widget_utils_textdomain)
@@ -134,7 +134,7 @@ class widget_utils {
 			. ' />'
 			. '</td>' . "\n"
 			. '</tr>' . "\n";
-		
+
 		echo '<tr valign="top">' . "\n"
 			. '<th scope="row">'
 			. __('Description', widget_utils_textdomain)
@@ -145,7 +145,7 @@ class widget_utils {
 			. '</textarea>'
 			. '</td>' . "\n"
 			. '</tr>' . "\n";
-		
+
 		echo '<tr valign="top">' . "\n"
 			. '<th scope="row">'
 			. __('Exclude', widget_utils_textdomain)
@@ -164,7 +164,7 @@ class widget_utils {
 			. '</label>'
 		 	. '</td>' . "\n"
 			. '</tr>' . "\n";
-		
+
 		echo '<tr valign="top">' . "\n"
 			. '<th scope="row">'
 			. '&nbsp;'
@@ -183,11 +183,11 @@ class widget_utils {
 			. '</label>'
 		 	. '</td>' . "\n"
 			. '</tr>' . "\n";
-		
+
 		echo '</table>' . "\n";
 	} # widget_config()
-	
-	
+
+
 	/**
 	 * save_widget_config()
 	 *
@@ -195,16 +195,16 @@ class widget_utils {
 	 * @param string $type
 	 * @return void
 	 **/
-	
-	function save_widget_config($post_ID, $type = null) {
+
+    static function save_widget_config($post_ID, $type = null) {
 		$post = get_post($post_ID);
-		
+
 		if ( $post->post_type == 'revision' || !$_POST || $post->post_type != $type )
 			return;
-		
+
 		if ( isset($_POST['widgets_exclude']) ) {
 			update_post_meta($post_ID, '_widgets_exclude', '1');
-			
+
 			if ( isset($_POST['widgets_exception']) )
 				update_post_meta($post_ID, '_widgets_exception', '1');
 			else
@@ -213,19 +213,19 @@ class widget_utils {
 			delete_post_meta($post_ID, '_widgets_exclude');
 			delete_post_meta($post_ID, '_widgets_exception');
 		}
-		
+
 		$label = addslashes(trim(strip_tags(stripslashes($_POST['widgets_label']))));
-		
+
 		if ( $label )
 			update_post_meta($post_ID, '_widgets_label', $label);
 		else
 			delete_post_meta($post_ID, '_widgets_label');
-		
+
 		if ( current_user_can('unfiltered_html') )
 			$desc = $_POST['widgets_desc'];
 		else
 			$desc = wp_filter_post_kses($_POST['widgets_desc']);
-		
+
 		if ( $desc )
 			update_post_meta($post_ID, '_widgets_desc', $desc);
 		else
